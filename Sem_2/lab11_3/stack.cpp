22b5b09416ab6ba8cbb6d6683130195fafef9d03
Aivaz Latypov <mr.aivazlatypov@yandex.ru>
2025-03-27 21:59:40 +0500

    Sem_2: Add lab11_3

---
+++ Sem_2/lab11_3/stack.cpp
@@ -0,0 +1,95 @@
+// 11. *char(строка символов), стек
+
+#include <cstring>
+#include <iostream>
+
+struct StackCharrElem
+{
+    char *data;
+    StackCharrElem *next;
+};
+
+struct CharrStack
+{
+    int size = 0;
+    StackCharrElem *top = nullptr;
+
+    void push(char *data)
+    {
+        StackCharrElem *elem = new StackCharrElem();
+        elem->data = data;
+        elem->next = top;
+        top = elem;
+        ++size;
+    }
+
+    char *pop()
+    {
+        char *result = top->data;
+        StackCharrElem *temp = top;
+        top = top->next;
+        --size;
+        delete temp;
+        return result;
+    }
+};
+
+CharrStack createStack(int size)
+{
+    CharrStack stack{};
+    char *data;
+    for (int i = 0; i < size; ++i)
+    {
+        data = new char[100];
+        std::cout << i + 1 << " element: ";
+        std::cin >> data;
+        stack.push(data);
+    }
+    return stack;
+}
+
+void showStack(CharrStack stack)
+{
+    std::cout << "Stack:\n";
+    for(StackCharrElem* elem = stack.top; elem != nullptr; elem = elem->next)
+    {
+        std::cout << elem->data << "\n";
+    }
+}
+
+int findElem(char* e, CharrStack stack)
+{
+    int i = 0;
+    for(StackCharrElem* elem = stack.top; elem != nullptr; elem = elem->next)
+    {
+        if(std::strcmp(elem->data, e) == 0)
+        {
+            return i;
+        }
+        ++i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int size;
+    std::cout << "Stack size: ";
+    std::cin >> size;
+    CharrStack stack = createStack(size);
+    showStack(stack);
+
+    char* elem = new char[100];
+    std::cout << "Enter new element to add: ";
+    std::cin >> elem;
+    stack.push(elem);
+    showStack(stack);
+
+    std::cout << "Enter element to Find: ";
+    elem = new char[100];
+    std::cin >> elem;
+
+    int result = findElem(elem, stack);
+    std::cout << "Result: " << result << '\n';
+    return 0;
+}
